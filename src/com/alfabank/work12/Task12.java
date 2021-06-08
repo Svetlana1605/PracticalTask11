96ad4abb8d149ee2acee41011c3687bcc27f0eee
Svetlana1605 <85413486+Svetlana1605@users.noreply.github.com>
2021-06-09 02:52:07 +0300

    Initial commit

---
+++ src/com/alfabank/work12/Task12.java
@@ -0,0 +1,17 @@
+package com.alfabank.work12;
+
+import java.util.Arrays;
+
+public class Task12 {
+    public static void main(String[] args) {
+        System.out.println(" ******          *        *           *        *         " );
+        System.out.println("       *        * *        *         *        * *         " );
+        System.out.println("       *       *   *        *       *        *   *        " );
+        System.out.println("       *      *     *        *     *        *     *       " );
+        System.out.println("*      *     *********        *   *        *********      " );
+        System.out.println(" *     *    *         *        * *        *         *     " );
+        System.out.println("   ****    *           *        *        *           *    " );
+
+
+    }
+}
---
+++ src/com/alfabank/work13/Task13.java
@@ -0,0 +1,12 @@
+package com.alfabank.work13;
+
+import java.util.Scanner;
+
+public class Task13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter name -> ");
+        String name = scanner.nextLine();
+        System.out.println("My name is " + name);
+    }
+}
